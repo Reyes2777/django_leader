8a12c42333fa7e5d74dcb56eaa68b0932d00668d
Reyes2777 <jonathan.reyes2777@gmail.com>
2019-08-03 17:53:56 -0500

    refactor model and enable admin for models

--- cities/models.py
+++ cities/models.py
@@ -5,11 +5,8 @@ class Region(models.Model):
     code = models.IntegerField(unique=True)
     name = models.CharField(max_length=30)
 
-    def __unicode__(self):
-        return self.code
-
-    class Meta:
-        ordering = ('code',)
+    def __str__(self):
+        return self.name
 
 
 class City(models.Model):
@@ -18,8 +15,5 @@ class City(models.Model):
     is_active = models.BooleanField()
     regions = models.ManyToManyField(Region)
 
-    def __unicode__(self):
-        return self.code
-
-    class Meta:
-        ordering = ('code',)
+    def __str__(self):
+        return self.name
