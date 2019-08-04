4bb70edf96ca45bd5c6995798cd22d87a76c2398
Reyes2777 <jonathan.reyes2777@gmail.com>
2019-08-03 19:08:56 -0500

    add documentation in models

--- cities/models.py
+++ cities/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Region(models.Model):
+    """
+    Model application regions:
+        .. code-block:: python3
+
+            code (int): id number unique
+            name (str): name of region
+    """
     code = models.IntegerField(unique=True)
     name = models.CharField(max_length=30)
 
@@ -10,6 +17,15 @@ class Region(models.Model):
 
 
 class City(models.Model):
+    """
+    Model application city:
+        .. code-block:: python3
+
+            code (int): id number unique
+            name (str): name of region
+            is_active (bool): is value of region is active or not
+            regions (list): list of region with relation many to many in regions
+    """
     code = models.IntegerField(unique=True)
     name = models.CharField(max_length=30)
     is_active = models.BooleanField()
